8f01abf8c4002d4b93a57afe643a4420ddeec6b5
earoglif <earoglif@gmail.com>
2022-07-06 13:38:09 +0300

    Move index.html to public dir

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
+            template: path.join(__dirname, 'src/public', 'index.html'),
         }),
     ],
     devServer: {
